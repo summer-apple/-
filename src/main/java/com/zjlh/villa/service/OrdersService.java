63ce382f3108863783711af52ded5b877c72d050
summer <summer.okay@gmail.com>
2015-11-10 00:37:10 +0800

    Fix

--- src/main/java/com/zjlh/villa/service/OrdersService.java
+++ src/main/java/com/zjlh/villa/service/OrdersService.java
@@ -150,7 +150,7 @@ logger.info(prePayReturn.getSign());
 		
 		params.put("total_fee",i+"");
 		params.put("spbill_create_ip", ip);
-		params.put("notify_url", "http://eliteeagle.org/villa/order/notify");
+		params.put("notify_url", "http://zhejianglehua.com/villa/order/notify");
 		params.put("openid", orders.getOpenid());
 		
 		String sign = getSign(params);
